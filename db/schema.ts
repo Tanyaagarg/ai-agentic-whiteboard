044ca86208b9b2c9b989fe8a2a4b7629e702159e
Tanya Garg <tgarg1168@gmail.com>
2026-09-01 15:15:45 +0530

    authorization and db setup

--- db/schema.ts
+++ db/schema.ts
@@ -4,6 +4,7 @@ export const users = pgTable("users", {
   id: serial("id").primaryKey(),
   name: text("name"),
   email: text("email").notNull().unique(),
+  credits: integer('credits').default(3),
   createdAt: timestamp("created_at").defaultNow().notNull(),
 });
 
