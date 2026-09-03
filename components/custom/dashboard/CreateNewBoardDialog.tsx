49b919946199c1f996342b84f5c39d14dc21ebc0
Tanya Garg <tgarg1168@gmail.com>
2026-09-03 11:39:38 +0530

    new workspace created

--- components/custom/dashboard/CreateNewBoardDialog.tsx
+++ components/custom/dashboard/CreateNewBoardDialog.tsx
@@ -7,57 +7,87 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-  DialogClose
+  DialogClose,
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
-import { Plus } from "lucide-react";
+import { Loader2, Plus } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { toast } from "@/components/ui/toast";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
 function CreateNewBoardDialog() {
-    
-    const [workspaceName,setWorkspaceName] = useState("");
+  const [workspaceName, setWorkspaceName] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [dialog, setDialog] = useState(false);
+  const route= useRouter();
 
-    const handleCreateBoard = () =>{
-        if(workspaceName.trim()==="" || workspaceName?.length>30){
-            toast.add({
-                type:"error",
-                title:"Invalid Workspace Name",
-                description: "Please enter a valid workspace name (1-30 characters)."
-            })
-        }
+  const handleCreateBoard = async () => {
+    if (workspaceName.trim() === "" || workspaceName?.length > 30) {
+      toast.add({
+        type: "error",
+        title: "Invalid Workspace Name",
+        description: "Please enter a valid workspace name (1-30 characters).",
+      });
+
+      return;
     }
+    setLoading(true);
+    const projectId = crypto.randomUUID();
+    const result = await axios.post("/api/projects", {
+      projectName: workspaceName,
+      projectId: projectId,
+    });
+
+    console.log(result?.data);
+    toast.add({
+      type: "success",
+      title: "New Workspace Created",
+    });
+    setLoading(false);
+    setDialog(false);
+    route.push('/workspace/' + projectId)
+  };
 
   return (
-      <Dialog>
-        <DialogTrigger>
-            <Button>
-                <Plus/>Create New Board
-            </Button>
-        </DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle className="text-lg font-bold">WhiteBoard Workspace Name</DialogTitle>
-          </DialogHeader>
-          <div>
-            <label className="text-gray-500">Enter WhiteBoard Workspace Name</label>
-            <Input placeholder="Workspace Name" className="mt-2"
-            onChange={(e)=>setWorkspaceName(e.target.value)}
-            />
-          </div>
+    <Dialog open={dialog} onOpenChange={setDialog}>
+      <DialogTrigger>
+        <Button>
+          <Plus />
+          Create New Board
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle className="text-lg font-bold">
+            WhiteBoard Workspace Name
+          </DialogTitle>
+        </DialogHeader>
+        <div>
+          <label className="text-gray-500">
+            Enter WhiteBoard Workspace Name
+          </label>
+          <Input
+            placeholder="Workspace Name"
+            className="mt-2"
+            onChange={(e) => setWorkspaceName(e.target.value)}
+          />
+        </div>
 
-          <DialogFooter>
-            <DialogClose>
-                <Button variant="outline">
-                    Cancel
-                </Button>
-            </DialogClose>
-            <Button disabled={workspaceName?.length==0} onClick={handleCreateBoard}>
-                Create
-            </Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
+        <DialogFooter>
+          <DialogClose>
+            <Button variant="outline">Cancel</Button>
+          </DialogClose>
+          <Button
+            disabled={workspaceName?.length == 0 || loading}
+            onClick={handleCreateBoard}
+          >
+            {loading && <Loader2 className='animate-spin' />}
+            Create
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 }
 
